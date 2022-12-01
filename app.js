711392cd8d60af7bf777411ce380ffda694b6d90
Hardik Hemant Panchal <74039863+Hardik-90@users.noreply.github.com>
2022-12-02 00:05:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
+const globalErrorHandler = require("./controllers/errorController");
+
+const app = express();
+
+app.use(cookieParser());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// express body parser
+app.use(express.json({ limit: "50kb" }));
+app.use(express.urlencoded({ extended: true, limit: "50kb" }));
+
+app.use(fileUpload());
+
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/orders", orderRoutes);
+app.use("/api/v1/cart", cartRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+console.log(`------------ ${process.env.NODE_ENV} ---------------`);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,43 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+const cloudinary = require("cloudinary");
+
+const mongoose = require("mongoose");
+
+process.on("uncaughtException", (err) => {
+   console.log("Unhandled Exception 💥 Shutting Down");
+   console.log(err.name, err.message, err.stack);
+   process.exit(1);
+});
+
+const DB = "mongodb://0.0.0.0:27017/ecommerece";
+
+cloudinary.config({
+   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+   api_key: process.env.CLOUDINARY_API_KEY,
+   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+mongoose
+   .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+   })
+   .then((con) => console.log("Database connection successful!"));
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () => {
+   console.log(`Server is up on port ${PORT} in ${process.env.NODE_ENV} mode`);
+});
+
+process.on("unhandledRejection", (err) => {
+   console.log("Unhandled Rejection 💥 Shutting Down");
+   console.log(err.name, err.message);
+   server.close(() => {
+      process.exit(1);
+   });
+});
