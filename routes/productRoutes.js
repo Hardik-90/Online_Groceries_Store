e2cf7cdde0195b21547d444b4086c7cb34fb4332
Hardik Hemant Panchal <74039863+Hardik-90@users.noreply.github.com>
2022-12-02 00:04:27 +0530

    routes added

---
+++ routes/productRoutes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const productController = require("../controllers/productController");
+const auth = require("../middlewares/auth");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(productController.getAllProducts)
+  .post(
+    auth.protect,
+    auth.restrictTo("admin"),
+    productController.createProduct
+  );
+
+router
+  .route("/admin")
+  .get(
+    auth.protect,
+    auth.restrictTo("admin"),
+    productController.getProductsForAdmin
+  );
+
+router
+  .route("/:id")
+  .get(productController.getProduct)
+  .patch(
+    auth.protect,
+    auth.restrictTo("admin"),
+    productController.updateProduct
+  )
+  .delete(
+    auth.protect,
+    auth.restrictTo("admin"),
+    productController.deleteProduct
+  );
+
+router
+  .route("/review/:id")
+  .get(productController.getProductReview)
+  .patch(auth.protect, productController.createProductReview)
+  .delete(auth.protect, productController.deleteReview);
+
+module.exports = router;
